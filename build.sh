3287692634c4e87931b31c977dae5bd5f3574c7a
Marco Mancuso <marco.mancuso@alfresco.com>
2017-11-13 14:18:46 +0000

    sync with alfresc-anaxes-shipyard

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 # package maven service artifact
-pom_location="service/pom.xml"
-artifact_version=$(mvn -f $pom_location org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)' 2> /dev/null)
-artifact_id=$(mvn -f $pom_location org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -Ev '(^\[|Download\w+:)' 2> /dev/null)
+
+output=$(printf 'ARTIFACT_VERSION=${project.version}\nARTIFACT_ID=${project.artifactId}\n0\n' | mvn -f service/pom.xml help:evaluate | grep -Ev '(^\[|Download\w+:)' 2> /dev/null)
+artifact_id=$(echo "$output" | grep '^ARTIFACT_ID' | cut -d = -f 2)
+artifact_version=$(echo "$output" | grep '^ARTIFACT_VERSION' | cut -d = -f 2)
+
 finalName=$artifact_id-$artifact_version'.jar'
 
-mvn -f $pom_location clean package
+mvn -f service/pom.xml clean package
 
 # build-push service docker image
 cp service/target/$finalName service-deployment/
@@ -12,9 +14,9 @@ cd service-deployment
 ./docker-build-push.sh $finalName $artifact_id $artifact_version
 rm $finalName
 
-build-push ui docker iomage
+#build-push ui docker iomage
 
-# cd ../
+cd ../
 cp -R ui ui-deployment/
 cd ui-deployment
 ./docker-build-push.sh $artifact_version
